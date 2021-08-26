d9db9bed70c54b001efd1d5139f13f3e7c028969
MrWakeCZ <joseff.kral@gmail.com>
2021-08-26 21:29:02 +0200

    Přidána podpora RGB do scoreboardu

--- src/main/java/cz/wake/manager/utils/scoreboard/ScoreboardManager.java
+++ src/main/java/cz/wake/manager/utils/scoreboard/ScoreboardManager.java
@@ -4,9 +4,12 @@ import cz.craftmania.craftcore.fastboard.FastBoard;
 import cz.wake.manager.Main;
 import cz.wake.manager.utils.configs.Config;
 import me.clip.placeholderapi.PlaceholderAPI;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class ScoreboardManager {
@@ -27,7 +30,8 @@ public class ScoreboardManager {
         try {
             FastBoard board = new FastBoard(player);
             board.updateTitle(this.boardName);
-            board.updateLines(this.boardLines);
+            List<String> finalLines = updateLines(this.boardLines, player.getPlayer());
+            board.updateLines(finalLines);
             boards.put(player.getUniqueId(), board);
         } catch (Exception exception) {
             exception.printStackTrace();
@@ -44,12 +48,44 @@ public class ScoreboardManager {
     public void update() {
         try {
             for (FastBoard board : this.boards.values()) {
-                List<String> lines = this.boardLines;
-                List<String> finalLines = lines.stream().map((line) -> PlaceholderAPI.setPlaceholders(board.getPlayer(), line)).collect(Collectors.toList());
+                List<String> finalLines = updateLines(this.boardLines, board.getPlayer());
                 board.updateLines(finalLines);
             }
         } catch (Exception exception) {
             exception.printStackTrace();
+        }
+    }
+
+    private List<String> updateLines(List<String> list, final Player player) {
+        return list.stream().map((line) -> PlaceholderAPI.setPlaceholders(player.getPlayer(), line)).map(this::miniMessageBuilder).collect(Collectors.toList());
+    }
+
+    private String miniMessageBuilder(String text) {
+        return MessageColorEdit.replaceHex(text);
+    }
+
+    //TODO: Přesunout do CraftLibs s lepší podporou
+    static class MessageColorEdit {
+
+        public static final Pattern HEX_PATTERN = Pattern.compile("&(#[A-Fa-f0-9]{6})");
+
+        public MessageColorEdit(){};
+
+        public static String color(String string) {
+            return string == null ? null : replaceHex(ChatColor.translateAlternateColorCodes('&', string));
+        }
+
+        public static String replaceHex(String str) {
+            Matcher matcher = HEX_PATTERN.matcher(str);
+            while (matcher.find()) {
+                str = str.replace(matcher.group(0), ChatColor.of(matcher.group(1)).toString());
+            }
+            return str;
+        }
+
+        public static List<String> colorAllLines(List<String> list) {
+            list.replaceAll(MessageColorEdit::color);
+            return list;
         }
     }
 }
