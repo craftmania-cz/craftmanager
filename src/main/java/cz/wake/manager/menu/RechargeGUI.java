a0665295b38539ad0be4fd003f74b22c04ba1621
MrWakeCZ <joseff.kral@gmail.com>
2022-06-29 00:39:59 +0200

    Fix: Oprava chybějící kontroly zda má hráč dostatek peněz na recharge

--- src/main/java/cz/wake/manager/menu/RechargeGUI.java
+++ src/main/java/cz/wake/manager/menu/RechargeGUI.java
@@ -6,6 +6,7 @@ import cz.craftmania.craftcore.inventory.builder.SmartInventory;
 import cz.craftmania.craftcore.inventory.builder.content.InventoryContents;
 import cz.craftmania.craftcore.inventory.builder.content.InventoryProvider;
 import cz.craftmania.crafteconomy.utils.VaultUtils;
+import cz.craftmania.craftlibs.utils.ChatInfo;
 import cz.craftmania.craftlibs.utils.ServerColors;
 import cz.craftmania.craftpack.api.Buttons;
 import org.bukkit.Bukkit;
@@ -23,6 +24,10 @@ public class RechargeGUI implements InventoryProvider {
         ItemStack money200 = new ItemBuilder(Material.FEATHER).setName(ServerColors.ROLE_DIAMOND.get() + "Obnovit: +1000")
                         .setLore("§eCena: §6$§f200", "", ServerColors.DARK_GRAY.get() + "Kliknutím si dobiješ fly").build();
         content.set(1, 2, ClickableItem.of(money200, (inventoryClickEvent -> {
+            if (vaultUtils.getBalance(player) < 200) {
+                ChatInfo.DANGER.send(player, "Nelze si zakoupit recharge, nemáš dostatek peněz §f$200");
+                return;
+            }
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "cmi flightcharge add %nick% 1000".replace("%nick%", player.getName()));
             vaultUtils.withdrawPlayer(player, 200);
             this.openRechargerGui(player);
@@ -31,6 +36,10 @@ public class RechargeGUI implements InventoryProvider {
         ItemStack money500 = new ItemBuilder(Material.FEATHER).setName(ServerColors.ROLE_DIAMOND.get() + "Obnovit: +2500")
                 .setLore("§eCena: §6$§f500", "", ServerColors.DARK_GRAY.get() + "Kliknutím si dobiješ fly").build();
         content.set(2, 2, ClickableItem.of(money500, (inventoryClickEvent -> {
+            if (vaultUtils.getBalance(player) < 500) {
+                ChatInfo.DANGER.send(player, "Nelze si zakoupit recharge, nemáš dostatek peněz §f$500");
+                return;
+            }
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "cmi flightcharge add %nick% 2500".replace("%nick%", player.getName()));
             vaultUtils.withdrawPlayer(player, 500);
             this.openRechargerGui(player);
@@ -39,6 +48,10 @@ public class RechargeGUI implements InventoryProvider {
         ItemStack money1000 = new ItemBuilder(Material.FEATHER).setName(ServerColors.ROLE_DIAMOND.get() + "Obnovit: +5300")
                 .setLore("§eCena: §6$§f1,000", "", ServerColors.DARK_GRAY.get() + "Kliknutím si dobiješ fly").build();
         content.set(3, 2, ClickableItem.of(money1000, (inventoryClickEvent -> {
+            if (vaultUtils.getBalance(player) < 1000) {
+                ChatInfo.DANGER.send(player, "Nelze si zakoupit recharge, nemáš dostatek peněz §f$1000");
+                return;
+            }
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "cmi flightcharge add %nick% 5300".replace("%nick%", player.getName()));
             vaultUtils.withdrawPlayer(player, 1000);
             this.openRechargerGui(player);
