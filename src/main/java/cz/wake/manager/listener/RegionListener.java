bab043c525f31aa88bd938c083ef3c4030d1215f
MrWakeCZ <joseff.kral@gmail.com>
2022-07-27 23:54:37 +0200

    Update: MagicCosmetics API

--- src/main/java/cz/wake/manager/listener/RegionListener.java
+++ src/main/java/cz/wake/manager/listener/RegionListener.java
@@ -1,7 +1,9 @@
 package cz.wake.manager.listener;
 
+import com.francobm.magicosmetics.api.Cosmetic;
 import com.francobm.magicosmetics.api.CosmeticType;
 import com.francobm.magicosmetics.api.MagicAPI;
+import com.francobm.magicosmetics.cache.PlayerData;
 import cz.craftmania.craftcore.events.worldguard.RegionEnterEvent;
 import cz.craftmania.craftcore.events.worldguard.RegionLeaveEvent;
 import cz.craftmania.craftlibs.utils.ChatInfo;
@@ -17,8 +19,8 @@ import java.util.List;
 
 public class RegionListener implements Listener {
 
-    private static final HashMap<Player, String> activeCosmeticBagpacks = new HashMap<>();
-    private static final HashMap<Player, String> activeCosmeticBalloons = new HashMap<>();
+    private static final HashMap<Player, Cosmetic> activeCosmeticBagpacks = new HashMap<>();
+    private static final HashMap<Player, Cosmetic> activeCosmeticBalloons = new HashMap<>();
     private static final List<Player> activeGlowing = new ArrayList<>();
 
     @EventHandler
@@ -27,16 +29,14 @@ public class RegionListener implements Listener {
             case "spawn-pinata" -> {
                 Player player = event.getPlayer();
                 if (MagicAPI.hasEquipCosmetic(player, CosmeticType.BALLOON)) {
-                    ItemStack itemStack = MagicAPI.getEquipped(player, CosmeticType.BALLOON);
+                    Cosmetic cosmetic = PlayerData.getPlayer(player).getBalloon();
+                    activeCosmeticBalloons.put(player, cosmetic);
                     MagicAPI.UnEquipCosmetic(player, CosmeticType.BALLOON);
-                    String cosmeticName = ChatColor.stripColor(itemStack.displayName().toString());
-                    activeCosmeticBalloons.put(player, getCosmeticIdByName(cosmeticName));
                     ChatInfo.INFO.send(player, "Nasazený balének ti byl odendán. Vstoupil jsi do chráněného regionu.");
                 }
                 if (MagicAPI.hasEquipCosmetic(player, CosmeticType.BAG)) {
-                    ItemStack itemStack = MagicAPI.getEquipped(player, CosmeticType.BAG);
-                    String cosmeticName = ChatColor.stripColor(itemStack.displayName().toString());
-                    activeCosmeticBagpacks.put(player, getCosmeticIdByName(cosmeticName));
+                    Cosmetic cosmetic = PlayerData.getPlayer(player).getBag();
+                    activeCosmeticBagpacks.put(player, cosmetic);
                     MagicAPI.UnEquipCosmetic(player, CosmeticType.BAG);
                     ChatInfo.INFO.send(player, "Nasazený batoh ti byl odendán. Vstoupil jsi do chráněného regionu.");
                 }
@@ -55,81 +55,17 @@ public class RegionListener implements Listener {
             case "spawn-pinata" -> {
                 Player player = event.getPlayer();
                 if (activeCosmeticBalloons.containsKey(player)) {
-                    MagicAPI.EquipCosmetic(player, activeCosmeticBalloons.get(player), "");
+                    PlayerData.getPlayer(player).setCosmetic(activeCosmeticBalloons.get(player));
                     activeCosmeticBalloons.remove(player);
                 }
                 if (activeCosmeticBagpacks.containsKey(player)) {
-                    MagicAPI.EquipCosmetic(player, activeCosmeticBagpacks.get(player), "");
+                    PlayerData.getPlayer(player).setCosmetic(activeCosmeticBagpacks.get(player));
                     activeCosmeticBagpacks.remove(player);
                 }
                 if (activeGlowing.contains(player)) {
                     activeGlowing.remove(player);
                     player.setGlowing(true);
                 }
-            }
-        }
-    }
-
-    private String getCosmeticIdByName(String cosmeticDisplayName) {
-        switch (cosmeticDisplayName) {
-            case "Axolotl" -> {
-                return "axolotl_backpack";
-            }
-            case "Backpack" -> {
-                return "back_backpack";
-            }
-            case "Crown Cape" -> {
-                return "crown_cape_backpack";
-            }
-            case "Golden Cape" -> {
-                return "golden_cape_backpack";
-            }
-            case "Heart Cape" -> {
-                return "heart_cape_backpack";
-            }
-            case "Cyber Wings" -> {
-                return "cyber_wings_backpack";
-            }
-            case "Dragon Tail" -> {
-                return "dragon_tail_backpack";
-            }
-            case "VIP Gold Cape" -> {
-                return "vip_gold_cape_backpack";
-            }
-            case "VIP Diamond Cape" -> {
-                return "vip_diamond_cape_backpack";
-            }
-            case "VIP Emerald Cape" -> {
-                return "vip_emerald_cape_backpack";
-            }
-            case "VIP Obsidian Cape" -> {
-                return "vip_obsidian_cape_backpack";
-            }
-            case "Tester Cape" -> {
-                return "tester_cape_surv118_backpack";
-            }
-            case "Kontrabas" -> {
-                return "bass_backpack";
-            }
-            case "Tortoise Shell" -> {
-                return "tortoise_shell";
-            }
-            case "Kraken Arms" -> {
-                return "kraken_arms";
-            }
-            case "Hotdog" -> {
-                return "hotdog";
-            }
-            case "Jednorožší kruh" -> {
-                return "unicorn_floaty";
-            }
-
-            // Balloons
-            case "Hot air balloon" -> {
-                return "hot_air_balloon";
-            }
-            default -> {
-                return null;
             }
         }
     }
